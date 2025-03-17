8d444836adb6e09ccc6bf3d5eb2ba4fdef9cb9f6
Franco <freconur@gmail.com>
2025-03-17 02:02:55 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const { auth, db } = require('./firebase')
 const app = express()
 const cors = require('cors')
 
-const whitelist = ['http://localhost:3001', 'http://localhost:3000', 'https://www.tuescuelagestiona.online/, https://tuescuelagestiona.online']
+// const whitelist = ['http://localhost:3001', 'http://localhost:3000', 'https://www.tuescuelagestiona.online/, https://tuescuelagestiona.online']
+const whitelist = ['https://www.tuescuelagestiona.online, https://tuescuelagestiona.online']
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
@@ -18,38 +19,42 @@ const options = {
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cors(whitelist))
+// app.use(cors(whitelist))
 
-
+const corsOptions = {
+  origin: 'https://www.tuescuelagestiona.online', // Solo permite solicitudes desde este dominio
+  methods: 'GET,POST',            // Solo permite estos métodos HTTP
+  allowedHeaders: ['Content-Type', 'Authorization'], // Permite ciertos encabezados
+};
 app.get('/', async (req, res) => {
   res.send('holiwis')
 })
 
-app.post('/crear-director', async (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'https://www.tuescuelagestiona.online')
-  // res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
-  const usuarioRef = db.collection('usuarios').doc(`${req.body.dni}`);
-  await usuarioRef.get()
-    .then(async doc => {
-      if (doc.exists === false) {
-        console.log('entroa a no existe')
-        const rta = await auth.createUser({
-          uid: req.body.dni,
-          email: req.body.email,
-          password: req.body.password,
-          emailVerified: false,
-          disabled: false
-        })
-        res.json({ ...rta, estado: true, warning: 'usuario creado con éxito', exists: false })
-      } else if (doc.exists === true) {
-        console.log('entroa a si existe')
-        res.json({ warning: 'usuario ya existe', estado: true, exists: true })
-      }
-    })
-})
+// app.post('/crear-director', async (req, res) => {
+//   res.header('Access-Control-Allow-Origin', 'https://www.tuescuelagestiona.online')
+//   // res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+//   const usuarioRef = db.collection('usuarios').doc(`${req.body.dni}`);
+//   await usuarioRef.get()
+//     .then(async doc => {
+//       if (doc.exists === false) {
+//         console.log('entroa a no existe')
+//         const rta = await auth.createUser({
+//           uid: req.body.dni,
+//           email: req.body.email,
+//           password: req.body.password,
+//           emailVerified: false,
+//           disabled: false
+//         })
+//         res.json({ ...rta, estado: true, warning: 'usuario creado con éxito', exists: false })
+//       } else if (doc.exists === true) {
+//         console.log('entroa a si existe')
+//         res.json({ warning: 'usuario ya existe', estado: true, exists: true })
+//       }
+//     })
+// })
 
-app.post('/crear-docente', async (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'https://www.tuescuelagestiona.online')
+app.post('/crear-docente',cors(corsOptions), async (req, res) => {
+  // res.header('Access-Control-Allow-Origin','https://www.tuescuelagestiona.online')
   // res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
 
   const usuarioRef = db.collection(`intituciones/${req.body.idInstitution}/usuarios`).doc(`${req.body.dni}`);
@@ -69,29 +74,29 @@ app.post('/crear-docente', async (req, res) => {
       }
     })
 })
-app.post('/borrar-usuario', async (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'https://www.tuescuelagestiona.online')
-  // res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
-  const usuarioRef = db.collection('usuarios').doc(`${req.body.dni}`);
-  await usuarioRef.get()
-    .then(async doc => {
-      if (!doc.exists) {
-        res.json({ warning: 'usuario no existe', estado: true, delete: false })
-      } else if (doc.exists) {
-        await db.collection('usuarios').doc(`${req.body.dni}`).delete()
-          .then(rta => {
-            console.log('res', rta)
-            auth.deleteUser(`${req.body.dni}`)
-              .then(() => {
-                console.log('Successfully deleted user');
-                res.json({ warning: 'se ha eliminado usuario con exito', estado: true, delete: true })
-              })
-              .catch((error) => {
-                console.log('Error deleting user:', error);
-              });
-          })
-      }
-    })
+// app.post('/borrar-usuario', async (req, res) => {
+//   res.header('Access-Control-Allow-Origin', 'https://www.tuescuelagestiona.online')
+//   // res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+//   const usuarioRef = db.collection('usuarios').doc(`${req.body.dni}`);
+//   await usuarioRef.get()
+//     .then(async doc => {
+//       if (!doc.exists) {
+//         res.json({ warning: 'usuario no existe', estado: true, delete: false })
+//       } else if (doc.exists) {
+//         await db.collection('usuarios').doc(`${req.body.dni}`).delete()
+//           .then(rta => {
+//             console.log('res', rta)
+//             auth.deleteUser(`${req.body.dni}`)
+//               .then(() => {
+//                 console.log('Successfully deleted user');
+//                 res.json({ warning: 'se ha eliminado usuario con exito', estado: true, delete: true })
+//               })
+//               .catch((error) => {
+//                 console.log('Error deleting user:', error);
+//               });
+//           })
+//       }
+//     })
 
-})
+// })
 module.exports = app
