74b388840d2562ded44cbed9822c38dd08c1461c
franco <freconur@gmail.com>
2025-04-02 01:37:29 -0500

    corrigiendo la exportascion de modular

--- src/dates.js
+++ src/dates.js
@@ -1,4 +1,7 @@
-export const months = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "setiembre", "octubre", "noviembre", "diciembre"]
-export const monthNumber = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
-// export const monthNumber = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
-export const days = ["domingo", "lunes", "martes", "miercoles", "jueves", "viernes", "sabado"]
+const months = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "setiembre", "octubre", "noviembre", "diciembre"]
+const monthNumber = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+//  const monthNumber = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+const days = ["domingo", "lunes", "martes", "miercoles", "jueves", "viernes", "sabado"]
+
+
+module.exports = { months, monthNumber, days }
