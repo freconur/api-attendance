f5ad40af72820fd1d85ee1c2b8e9f232b4a5741e
franco <freconur@gmail.com>
2025-04-02 01:33:02 -0500

    agregando sistema cron para automatizar las creaciones de rutas de fechas diarios y la actualizacion de los usurioa estudiantes que hayan faltado en la fecha

--- src/app.js
+++ src/app.js
@@ -1,34 +1,37 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require("express");
+const morgan = require("morgan");
 // const admin = require("firebase-admin");
-const { auth, db } = require('./firebase')
-const app = express()
-const cors = require('cors')
-
+const { auth, db } = require("./firebase");
+const app = express();
+const cors = require("cors");
+const { months, days } = require("./dates");
+const cron = require("node-cron")
 // const whitelist = ['http://localhost:3001', 'http://localhost:3000', 'https://www.tuescuelagestiona.online/, https://tuescuelagestiona.online']
-const whitelist = ['https://www.tuescuelagestiona.online, https://tuescuelagestiona.online']
+const whitelist = [
+  "https://www.tuescuelagestiona.online, https://tuescuelagestiona.online",
+];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('no permitido'));
+      callback(new Error("no permitido"));
     }
-  }
-}
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cors({origin:'https://www.tuescuelagestiona.online'}))
+  },
+};
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors({ origin: "https://www.tuescuelagestiona.online" }));
 
 const corsOptions = {
-  origin: 'https://www.tuescuelagestiona.online', // Solo permite solicitudes desde este dominio
-  methods: 'GET,POST',            // Solo permite estos métodos HTTP
-  allowedHeaders: ['Content-Type', 'Authorization'], // Permite ciertos encabezados
+  origin: "https://www.tuescuelagestiona.online", // Solo permite solicitudes desde este dominio
+  methods: "GET,POST", // Solo permite estos métodos HTTP
+  allowedHeaders: ["Content-Type", "Authorization"], // Permite ciertos encabezados
 };
-app.get('/', async (req, res) => {
-  res.send('holiwis')
-})
+app.get("/", async (req, res) => {
+  res.send("holiwis");
+});
 
 // app.post('/crear-director', async (req, res) => {
 //   res.header('Access-Control-Allow-Origin', 'https://www.tuescuelagestiona.online')
@@ -53,50 +56,195 @@ app.get('/', async (req, res) => {
 //     })
 // })
 
-app.post('/crear-docente', async (req, res) => {
+app.post("/crear-docente", async (req, res) => {
   // res.header('Access-Control-Allow-Origin','https://www.tuescuelagestiona.online')
   // res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
 
-  const usuarioRef = db.collection(`intituciones/${req.body.idInstitution}/usuarios`).doc(`${req.body.dni}`);
-  await usuarioRef.get()
-    .then(async doc => {
-      if (!doc.exists) {
-        const rta = await auth.createUser({
-          uid: req.body.dni,
-          email: req.body.email,
-          password: req.body.password,
-          emailVerified: false,
-          disabled: false
+  const usuarioRef = db
+    .collection(`intituciones/${req.body.idInstitution}/usuarios`)
+    .doc(`${req.body.dni}`);
+  await usuarioRef.get().then(async (doc) => {
+    if (!doc.exists) {
+      const rta = await auth.createUser({
+        uid: req.body.dni,
+        email: req.body.email,
+        password: req.body.password,
+        emailVerified: false,
+        disabled: false,
+      });
+      res.json({
+        ...rta,
+        estado: true,
+        warning: "usuario creado con éxito",
+        exists: false,
+      });
+    } else if (doc.exists) {
+      res.json({ warning: "usuario ya existe", estado: true, exists: true });
+    }
+  });
+});
+
+cron.schedule('15 16 * * *', () => {
+// app.get("/update-attendance", async (req, res) => {
+  // el id de la institucion por ahora sera estitico pero cuando se agregen mas colegios se hara dinamico
+  //crearemos una nueva instancia para la fecha actual
+  const currentDate = new Date();
+  //hago un get de todos los estudiantes de la institucion
+  if (days[currentDate.getDay()] !== 'sabado' || days[currentDate.getDay()] !== 'domingo') {
+
+    const usuarioRef = db.collection(
+      `/intituciones/l2MjRJSZU2K6Qdyc3lUz/students`
+    );
+
+    const newPromise = new Promise(async (resolve, recject) => {
+      try {
+        await usuarioRef.get().then((rtaEstudiantes) => {
+          //creo un array vacio para poder almacenar todos los datos obtenidos del get
+          console.log('rtaEstudiantes', rtaEstudiantes)
+          const arrayEstudiantes = [];
+          //creamos un id con el nombre de index para poder identificar el ultimo elemento del array
+          let index = 0
+          rtaEstudiantes.forEach((doc) => {
+            arrayEstudiantes.push({ ...doc.data(), id: doc.id });
+            // console.log('rtaEstudiantes.size', rtaEstudiantes.size)
+            // console.log('index', index)
+            index = index + 1
+            if (rtaEstudiantes.size === index) {
+              console.log('aqui quedamos')
+              resolve(arrayEstudiantes)
+            }
+          });
+        });
+      } catch (error) {
+        console.log("error", error);
+        recject();
+      }
+    });
+
+
+    const validarAsistenciaPromise = new Promise((resolve, reject) => {
+
+      let index = 0
+      try {
+        newPromise.then(rtaDeEstudiantes => {
+          //una vez obtenido los id de todos los estudiantes de la institucion procederemos a verificar si tienen registro de asistencia tanto de entrada como de salida
+          const arrayEstudiantesData = []
+          rtaDeEstudiantes.forEach(async (estudiante) => {
+            const asistenciaPath = `/intituciones/l2MjRJSZU2K6Qdyc3lUz/attendance-student/${estudiante.dni}/${currentDate.getFullYear()}/${months[currentDate.getMonth()]}/${months[currentDate.getMonth()]}`;
+            // const asistenciaPath = `/intituciones/l2MjRJSZU2K6Qdyc3lUz/attendance-student/${estudiante.dni}/${currentDate.getFullYear()}/marzo/marzo`; //esto esta hardcodeado para poder actualizar los datos del mes de marzo
+            const asistenciaRef = db
+              .collection(asistenciaPath)
+              .doc(`${currentDate.getDate()}`);
+            // .doc(`${currentDate.getDate()}`);
+
+            await asistenciaRef.get()
+              .then(async (asistencia) => {
+                // console.log('asistencia.data()', asistencia.data())
+                if (asistencia.exists) {
+                  if (
+                    asistencia.data().arrivalTime === undefined &&
+                    asistencia.data().departure === undefined
+                  ) {
+                    index = index + 1
+                    // console.log("estudiante ha faltado, se le pondra como falta para esta fecha");
+                    arrayEstudiantesData.push({ ...asistencia.data(), id: estudiante.dni, llegada: 'tarde' })
+                    const rutaDeNuevoMes = db.collection(asistenciaPath).doc(`${currentDate.getDate()}`);
+
+                    await rutaDeNuevoMes.set({
+                      // arrivalTime: 'falto',
+                      falta: true
+                    }, { merge: true });
+                    if (rtaDeEstudiantes.length === index) {
+                      resolve(true)
+                    }
+                  } else {
+                    index = index + 1
+                    // console.log(`dni:${asistencia.data().dni}, ingreso: ${asistencia.data()}`)
+                    arrayEstudiantesData.push({ ...asistencia.data(), id: estudiante.dni, llegada: 'temprano' })
+                    if (rtaDeEstudiantes.length === index) {
+                      resolve(true)
+                    }
+                  }
+                } else {
+                  console.log("estudiante no existe");
+                }
+
+              })
+          });
+
         })
-        res.json({ ...rta, estado: true, warning: 'usuario creado con éxito', exists: false })
-      } else if (doc.exists) {
-        res.json({ warning: 'usuario ya existe', estado: true, exists: true })
+      } catch (error) {
+        console.log('error', error)
+        reject()
       }
     })
-})
-// app.post('/borrar-usuario', async (req, res) => {
-//   res.header('Access-Control-Allow-Origin', 'https://www.tuescuelagestiona.online')
-//   // res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
-//   const usuarioRef = db.collection('usuarios').doc(`${req.body.dni}`);
-//   await usuarioRef.get()
-//     .then(async doc => {
-//       if (!doc.exists) {
-//         res.json({ warning: 'usuario no existe', estado: true, delete: false })
-//       } else if (doc.exists) {
-//         await db.collection('usuarios').doc(`${req.body.dni}`).delete()
-//           .then(rta => {
-//             console.log('res', rta)
-//             auth.deleteUser(`${req.body.dni}`)
-//               .then(() => {
-//                 console.log('Successfully deleted user');
-//                 res.json({ warning: 'se ha eliminado usuario con exito', estado: true, delete: true })
-//               })
-//               .catch((error) => {
-//                 console.log('Error deleting user:', error);
-//               });
-//           })
-//       }
-//     })
+    validarAsistenciaPromise.then(response => {
+      // console.log('response', response.filter(a => a.llegada === 'tarde'))
 
+      // response === true && res.send("holiwis");
+      response === true && console.log("holiwis");
+    })
+  }
+});
 // })
-module.exports = app
+
+
+// app.get("/create-route", async (req, res) => {
+// el id de la institucion por ahora sera estitico pero cuando se agregen mas colegios se hara dinamico
+//crearemos una nueva instancia para la fecha actual
+cron.schedule('11 1 * * *', () => {
+  const currentDate = new Date();
+  if (days[currentDate.getDay()] !== 'sabado' || days[currentDate.getDay()] !== 'domingo') {
+    //hago un get de todos los estudiantes de la institucion
+    const usuarioRef = db.collection(
+      `/intituciones/l2MjRJSZU2K6Qdyc3lUz/students`
+    );
+
+    const newPromise = new Promise(async (resolve, recject) => {
+      try {
+        await usuarioRef.get().then((rtaEstudiantes) => {
+          //creo un array vacio para poder almacenar todos los datos obtenidos del get
+          console.log('rtaEstudiantes', rtaEstudiantes)
+          const arrayEstudiantes = [];
+          //creamos un id con el nombre de index para poder identificar el ultimo elemento del array
+          let index = 0
+          rtaEstudiantes.forEach((doc) => {
+            arrayEstudiantes.push({ ...doc.data(), id: doc.id });
+            // console.log('rtaEstudiantes.size', rtaEstudiantes.size)
+            // console.log('index', index)
+            index = index + 1
+            if (rtaEstudiantes.size === index) {
+              console.log('aqui quedamos')
+              resolve(arrayEstudiantes)
+            }
+          });
+        });
+      } catch (error) {
+        console.log("error", error);
+        recject();
+      }
+    });
+
+    newPromise.then(async (estudiantes) => {
+      let index = 0
+      estudiantes.forEach(async (estudiante) => {
+        const asistenciaPath = `/intituciones/l2MjRJSZU2K6Qdyc3lUz/attendance-student/${estudiante.dni}/${currentDate.getFullYear()}/${months[currentDate.getMonth()]}/${months[currentDate.getMonth()]}`;
+        index = index + 1
+        // const asistenciaPath = `/intituciones/l2MjRJSZU2K6Qdyc3lUz/attendance-student/${estudiante.dni}/${currentDate.getFullYear()}/marzo/marzo`;
+        const rutaDeNuevoMes = db.collection(asistenciaPath).doc(`${currentDate.getDate()}`);
+
+        await rutaDeNuevoMes.set({
+          // active: true
+        }, { merge: true });
+      })
+      if (estudiantes.length === index) {
+        // res.send("terminamos")
+        console.log("terminamos")
+      }
+    })
+  }
+
+})
+// })
+
+module.exports = app;
---
+++ src/dates.js
@@ -0,0 +1,4 @@
+export const months = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "setiembre", "octubre", "noviembre", "diciembre"]
+export const monthNumber = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+// export const monthNumber = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+export const days = ["domingo", "lunes", "martes", "miercoles", "jueves", "viernes", "sabado"]
